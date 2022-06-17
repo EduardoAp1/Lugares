cec0fb3efea2e49a0c4564be902c241a004415c2
Eduardo <eacuna40450@ufide.ac.cr>
2022-06-17 14:21:03 -0600

    Semana 05,envio de correo, realizar llamdas, entrar a la web y ubicacion del lugar

--- app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -1,10 +1,13 @@
 package com.lugares.ui.lugar
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -39,13 +42,105 @@ class UpdateLugarFragment : Fragment() {
         binding.etCorreo.setText(args.lugar.correo)
         binding.etTelefono.setText(args.lugar.telefono)
         binding.etWeb.setText(args.lugar.web)
+        binding.tvAltura.text=args.lugar.altura.toString()
+        binding.tvLatitud.text=args.lugar.latitud.toString()
+        binding.tvLongitud.text=args.lugar.longitud.toString()
 
         binding.btActualizar.setOnClickListener{
             updateLugar()
         }
 
+        binding.btEmail.setOnClickListener(){
+            escribirCorreo()
+        }
+
+        binding.btPhone.setOnClickListener(){
+            realizarLlamada()
+        }
+
+        binding.btWeb.setOnClickListener(){
+            verWeb()
+        }
+
+        //Se indica que esta pantalla tiene un menu personalizado....
+        setHasOptionsMenu(true)
+
+
         return binding.root
     }
+
+    private fun verWeb() {
+        val recurso = binding.etWeb.text.toString()
+        if (recurso.isNotEmpty()){
+            val accion = Intent(Intent.ACTION_VIEW,Uri.parse("http://$recurso"))
+            startActivity(accion) //Efectivamente se carga el app de correo
+        }else{
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun realizarLlamada() {
+        val recurso = binding.etTelefono.text.toString()
+        if (recurso.isNotEmpty()){
+            val accion = Intent(Intent.ACTION_CALL)
+            accion.data = Uri.parse("tel:$recurso")
+            if (requireActivity().checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+                //Si no se ha otorgado el permiso de hacer llamadas... que se pida el permiso
+                requireActivity().requestPermissions(arrayOf(Manifest.permission.CALL_PHONE),105)
+            }else{ //Si se tiene permiso realice la llamada
+                requireActivity().startActivity(accion)
+            }
+        }else{
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun escribirCorreo() {
+        val recurso = binding.etCorreo.text.toString()
+        if (recurso.isNotEmpty()){
+            val accion = Intent(Intent.ACTION_SEND)
+            accion.type = "message/rfc822"
+            accion.putExtra(Intent.EXTRA_EMAIL, arrayOf(recurso))
+            accion.putExtra(Intent.EXTRA_SUBJECT,
+                getString(R.string.msg_saludos)+" "+binding.etNombre.text
+            )
+            accion.putExtra(Intent.EXTRA_TEXT,getString(R.string.msg_mensaje_correo))
+            startActivity(accion) //Efectivamente se carga el app de correo
+        }else{
+            Toast.makeText(requireContext(),getString(R.string.msg_datos),
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //Consulto si se dio click en el icono de borrar
+        if(item.itemId==R.id.menu_delete){
+            deleteLugar()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun deleteLugar() {
+        val pantalla = AlertDialog.Builder(requireContext())
+
+        pantalla.setTitle(R.string.delete)
+        pantalla.setMessage(getString(R.string.seguroBorrar)+" ${args.lugar.nombre}?")
+
+        pantalla.setPositiveButton(getString(R.string.si)){ _,_->
+            lugarViewModel.deleteLugar(args.lugar)
+            findNavController().navigate(R.id.action_updateLugarFragment_to_nav_lugar)
+        }
+
+        pantalla.setNegativeButton(getString(R.string.no)){ _,_->}
+        pantalla.create().show()
+    }
+
 
     private fun updateLugar() {
         val nombre = binding.etNombre.text.toString()
