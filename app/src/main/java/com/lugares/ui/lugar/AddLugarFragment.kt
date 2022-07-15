e9e48499c04ef838a00f8ec645391fc134f65285
Eduardo <eacuna40450@ufide.ac.cr>
2022-07-14 20:54:22 -0600

    Semana 09, Storage fotos y audios

--- app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
+++ app/src/main/java/com/lugares/ui/lugar/AddLugarFragment.kt
@@ -1,28 +1,46 @@
 package com.lugares.ui.lugar
 
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.play.core.install.model.ActivityResult
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 import com.lugares.R
 import com.lugares.databinding.FragmentAddLugarBinding
 import com.lugares.model.Lugar
+import com.lugares.utiles.AudioUtiles
+import com.lugares.utiles.ImagenUtiles
 import com.lugares.viewmodel.LugarViewModel
 
 class AddLugarFragment : Fragment() {
 
     private var _binding: FragmentAddLugarBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var audioUtiles: AudioUtiles
+    private lateinit var imagenUtiles: ImagenUtiles
+    private lateinit var tomarFotosActivity: ActivityResultLauncher<Intent>
 
     private lateinit var lugarViewModel: LugarViewModel
 
@@ -37,13 +55,85 @@ class AddLugarFragment : Fragment() {
         _binding = FragmentAddLugarBinding.inflate(inflater, container, false)
 
         binding.btAgregar.setOnClickListener{
-            addLugar()
+            //addLugar()
+            binding.progressBar.visibility = ProgressBar.VISIBLE
+            binding.msgMensaje.text = getString(R.string.msg_subiendo_audio)
+            binding.msgMensaje.visibility = TextView.VISIBLE
+            subeAudio()
         }
 
         ubicaGPS()
 
+        audioUtiles = AudioUtiles(requireActivity(),
+        requireContext(),
+        binding.btAccion,
+        binding.btPlay,
+        binding.btDelete,
+        getString(R.string.msg_graba_audio),
+        getString(R.string.msg_detener_audio))
+
+        tomarFotosActivity = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if(result.resultCode ==  Activity.RESULT_OK){
+                imagenUtiles.actualizaFoto()
+            }
+        }
+
+        imagenUtiles =  ImagenUtiles(
+            requireContext(),
+            binding.btPhoto,
+            binding.btRotaL,
+            binding.btRotaR,
+            binding.imagen,
+            tomarFotosActivity)
 
         return binding.root
+    }
+
+    private fun subeAudio() {
+        val audioFile = audioUtiles.audioFile
+        if(audioFile.exists() && audioFile.isFile && audioFile.canRead()){
+            val ruta =  Uri.fromFile(audioFile)
+            val rutaNube = "lugaresApp/${Firebase.auth.currentUser?.email}/audios/${audioFile.name}"
+            val refencia: StorageReference = Firebase.storage.reference.child(rutaNube)
+            refencia.putFile(ruta)
+                .addOnSuccessListener {
+                    refencia.downloadUrl
+                        .addOnSuccessListener {
+                            val rutaAudio = it.toString()
+                            subeImagen(rutaAudio)
+                        }
+                }
+                .addOnFailureListener {
+                    subeImagen("")
+                }
+        }else{
+            subeImagen("")
+        }
+    }
+
+    private fun subeImagen(rutaAudio: String) {
+        binding.msgMensaje.text = getString(R.string.msg_subiendo_imagen)
+        val imagenFile = imagenUtiles.imagenFile
+        if(imagenFile.exists() && imagenFile.isFile && imagenFile.canRead()){
+            val ruta =  Uri.fromFile(imagenFile)
+            val rutaNube = "lugaresApp/${Firebase.auth.currentUser?.email}/imagenes/${imagenFile.name}"
+            val refencia: StorageReference = Firebase.storage.reference.child(rutaNube)
+            refencia.putFile(ruta)
+                .addOnSuccessListener {
+                    refencia.downloadUrl
+                        .addOnSuccessListener {
+                            val rutaImagen = it.toString()
+                            addLugar(rutaAudio,rutaImagen)
+                        }
+                }
+                .addOnFailureListener {
+                    addLugar(rutaAudio, "")
+                }
+        }else{
+            addLugar(rutaAudio,"")
+        }
     }
 
     private fun ubicaGPS() {
@@ -75,7 +165,7 @@ class AddLugarFragment : Fragment() {
         }
     }
 
-    private fun addLugar() {
+    private fun addLugar(rutaAudio: String,rutaImagen: String) {
         val nombre = binding.etNombre.text.toString()
         val correo = binding.etCorreo.text.toString()
         val telefono = binding.etTelefono.text.toString()
@@ -85,7 +175,7 @@ class AddLugarFragment : Fragment() {
         val altura = binding.tvAltura.text.toString().toDouble()
 
         if(nombre.isNotEmpty()){ //Crea el lugar
-            val lugar= Lugar("",nombre,correo,telefono,web,latitud,longitud,altura,"","")
+            val lugar= Lugar("",nombre,correo,telefono,web,latitud,longitud,altura,rutaAudio,rutaImagen)
             lugarViewModel.saveLugar(lugar)
             Toast.makeText(requireContext(),getString(R.string.msg_lugar_added),Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_addLugarFragment_to_nav_lugar)
